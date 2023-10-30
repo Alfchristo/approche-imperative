7b2904846a0b3c39dc05eb800932a524f3322ba6
Alfchristo <juppchristophe@hotmail.com>
2023-10-30 17:18:08 +0100

    - v2

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,7 +1,7 @@
 package fr.declaration.variable;
 
 public class DeclarationApp { public static void main(String args[])
-        { byte demoByte = 30;
+        {   byte demoByte = 30;
             short demoShort = 15325;
             int demoInt = 30102023;
             long demoLong = 365L;
@@ -10,7 +10,16 @@ public class DeclarationApp { public static void main(String args[])
             char demoChar = 'D';
             boolean demoBoolean = true;
             String demoString = "Declaration App";
+
+            System.out.println(demoByte);
+            System.out.println(demoShort);
+            System.out.println(demoInt);
+            System.out.println(demoLong);
+            System.out.println(demoFloat);
             System.out.println(demoDouble);
+            System.out.println(demoChar);
+            System.out.println(demoBoolean);
+            System.out.println(demoString);
             String randomString = "Voici le résultat d'un calcul:\n" +
                     "1+5=6";
             System.out.println(randomString);
