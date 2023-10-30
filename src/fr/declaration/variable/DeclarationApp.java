4ff81a555cd6b4f7546f3260837df1def94ffbbc
Alfchristo <juppchristophe@hotmail.com>
2023-10-30 17:15:02 +0100

    - v2

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,18 @@
+package fr.declaration.variable;
+
+public class DeclarationApp { public static void main(String args[])
+        { byte demoByte = 30;
+            short demoShort = 15325;
+            int demoInt = 30102023;
+            long demoLong = 365L;
+            float demoFloat = 36.5F;
+            double demoDouble = 36.5D;
+            char demoChar = 'D';
+            boolean demoBoolean = true;
+            String demoString = "Declaration App";
+            System.out.println(demoDouble);
+            String randomString = "Voici le résultat d'un calcul:\n" +
+                    "1+5=6";
+            System.out.println(randomString);
+}
+}
